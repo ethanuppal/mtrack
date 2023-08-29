6d5a5954e1855feff9c96e50c9842fc2b69ebf86
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2023-08-29 01:03:23 -0400

    Update errors.c

--- mtrace/errors.c
+++ mtrace/errors.c
@@ -1,12 +1,13 @@
 // malloc-tracker: errors.c
 // Copyright (C) 2023 Ethan Uppal. All rights reserved.
 
+#include <stdio.h>
 #include "errors.h"
 
 void message(enum domain, const char* msg, const char* fix) {
     printf("error: %s.", msg);
     if (fix) {
-        printf("%s.", fix);
+        printf(" %s.", fix);
     }
     putchar('\n');
 }
